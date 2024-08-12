9664a3bcf76ac1e1d0ec459529fea39e9f7b0cd8
suryakant thombare <suryakant.thombare@ispace.com>
2024-08-12 12:15:56 +0530

    quizapp

---
+++ Quizapp/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+ import Quiz from './components/Quiz.jsx'
+ import "./App.css"
+function App() {
+  return (
+    <>
+      { <Quiz/> }
+      
+      </>
+  )
+}
+
+export default App
